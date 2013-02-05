bf9242331206724983ae505bbdc414a4f642ba61
Birkir A. Barkarson <birkirb@stoicviking.net>
2013-02-05 23:45:02 +0900

    Network info on bridge.

--- lib/hue/extensions/bridge.rb
+++ lib/hue/extensions/bridge.rb
@@ -5,8 +5,17 @@ module Hue
       state = self.status
       config = state['config']
       puts "#{config['name']}"
-      puts "IP: #{config['ipaddress']}"
       puts "Button pressed: #{config['linkbutton']}"
+      puts "Timestamp: #{config['UTC']}"
+      puts "Network configuration:"
+      puts "  IP:      #{config['ipaddress']}"
+      puts "  Gateway: #{config['gateway']}"
+      puts "  Mask:    #{config['netmask']}"
+      puts "  DHCP:    #{config['dhcp']}"
+      puts "  MAC:     #{config['mac']}"
+      if !config['proxyaddress'].strip.empty?
+        puts "  Proxy:   #{config['proxyaddress']}:#{config['proxyport']}"
+      end
       puts "Applications:"
       config['whitelist'].each do |key, values|
         puts "  #{key} : #{values['name']}"
