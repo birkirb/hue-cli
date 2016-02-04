0ff8d9e3d4f44094c15968e2131a3be74ef7ee32
Coby Randquist <randquistcp@gmail.com>
2016-02-04 08:58:24 -0800

    added formatted printing to keep things in columns added a check for hue attribute before attempting to display color state"

--- lib/hue/extensions/bulb.rb
+++ lib/hue/extensions/bulb.rb
@@ -9,7 +9,7 @@ module Hue
       puts "#{self.id}.".ljust(spaces) + "#{self.name}: #{self.on? ? 'ON' : 'OFF'}"
       indent(spaces) do
         puts "Brightness: #{self.brightness} (#{(self.brightness_percent).to_i}%)"
-        puts "Color: #{self.color}, #{self.color.to_rgb}"
+        puts "Color: #{self.color}, #{self.color.to_rgb}" if self.state["hue"]
         transitory_state = [
           (self.transition_time > 0 ? "Transition time: #{self.transition_time} sec" : nil),
           (self.blinking? ? 'Alert: Blinking!' : nil),
